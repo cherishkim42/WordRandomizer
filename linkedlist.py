ab5a771858a9eb2a910123aee04daa491749cd24
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-11-27 15:09:18 -0800

    listogram fixed

--- linkedlist.py
+++ linkedlist.py
@@ -70,7 +70,6 @@ class LinkedList(object):
             self.head = node
             self.tail = node #If a linked list has 1 item, the node refers to tail as well as head
         else: #Linked list isn't empty
-            assert self.tail is not None #For safety
             self.tail.next = node #Previous tail points us in the direction of new node
             self.tail = node #Set tail to node
 
@@ -78,10 +77,12 @@ class LinkedList(object):
         """Insert the given item at the head of this linked list.
         TODO: Running time: O(???) Why and under what conditions?"""
         node = Node(item)
-        node.next = self.head
-        self.head = node
-        if self.tail is not None:
+        if self.is_empty():
+            self.head = node
             self.tail = node
+        else: #Linked list isn't empty
+            node.next = self.head
+            self.head = node
 
     def find(self, quality):
         """Return an item from this linked list satisfying the given quality.
@@ -89,13 +90,17 @@ class LinkedList(object):
         TODO: Worst case running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all nodes to find item where quality(item) is True
         # TODO: Check if node's data satisfies given quality function
-        node = self.head
-        while node is not None:
-            if quality(node.data) is True:
-                return node.data
-            else:
-                node = node.next
-        return None
+        if self.is_empty():
+            print("{} is not in linked list".format(quality))
+            return None
+        else: #Linked list isn't empty
+            current_node = self.head
+            for i in range(0, self.length()):
+                if quality(current_node.data): #Quality: Are conditions being met?
+                    return current_node.data
+                else: #Those conditions aren't being met? Let's keep going until they are
+                    current_node = current_node.next
+            return current_node
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
@@ -105,34 +110,35 @@ class LinkedList(object):
         # TODO: Update previous node to skip around node with matching data
         # TODO: Otherwise raise error to tell user that delete has failed
         # Hint: raise ValueError('Item not found: {}'.format(item))
-        previous_node = None
-        current_node = self.head
+        if self.is_empty():
+            raise ValueError("Linked list is empty")
 
-        while current_node is not None:
-            if current_node.data == item:
-                if self.head == current_node:
-                    self.head == current_node.next #Reassign head
+        if self.head.data == item: #Checks if linked list contains only one word
+            self.head = self.head.next #Set forward
+            if self.tail.data == item:
+                self.tail = None
+            return
+
+        current_node = self.head
+        previous_node = None
+        while current_node: #Checks all nodes in list until current_node.data is equal to passed-in argument item
+            if current_node.data != item:
+                previous_node = current_node
+                current_node = current_node.next
+            else:
+                if self.tail.data == item: #Dingdingding they're the same! Set previous node to None and reinitialize tail
+                    if previous_node:
+                        previous_node.next = None
+                        self.tail = previous_node
+                        return
+                    else:
+                        self.head = None
+                        self.tail = None
+                        return
                 else:
                     previous_node.next = current_node.next
-                if self.tail == current_node:
-                    self.tail = previous_node #Reassign tail
-                current_node = None #Delete current_node's data
-                return
-        else: #Current node != item
-            previous_node = current_node #Remember previous node
-            current_node = current_node.next #Set current node to next node to keep iterating
-
-        raise ValueError('Item not found: {}'.format(item)) #Raise error if item never found
-
-    def replace(self, orig_value, new_value):
-        # walk through ll until finding orig_value. If found, replace with new_value
-        node = self.head
-        while node is not None: # ll isn't empty & node not out of range
-            if node.data == orig_value:
-                node.data = new_value
-            else: # keep looking
-                node = node.next
-        raise ValueError('Replacement target not found: {}'.format(target))
+                    return
+        raise ValueError('Item not found: {}'.format(item))
 
 def test_linked_list():
     ll = LinkedList()
